77444200713786b4b160437d3d46d5ad3cdb30ea
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:36:58 +0300

    commit solution of 15th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -142,3 +142,14 @@ first_name,
 last_name DESC,
 middle_name,
 employee_id;
+
+/*
+15.	Create View Employees with Salaries
+Write a SQL query to create a view v_employees_salaries with first name, last name and salary for each employee. 
+Submit your query statements as Run skeleton, run queries & check DB. 
+*/
+
+CREATE VIEW `v_employees_salaries` 
+AS SELECT first_name, last_name, salary FROM employees;
+
+SELECT * FROM v_employees_salaries;

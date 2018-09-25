c5d929fe3ab771243e8ab18d972424d4fa938ee3
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 11:42:11 +0300

    commit solution of 19th task

--- 01. Introduction To Databases/Intruduction to Databases.sql
+++ 01. Introduction To Databases/Intruduction to Databases.sql
@@ -535,3 +535,19 @@ SELECT * FROM departments
 ORDER BY name;
 SELECT * FROM employees
 ORDER BY salary DESC;
+
+# 19.	Basic Select Some Fields
+/*
+Modify queries from previous problem to show only some of the columns. For table:
+•	towns – name
+•	departments – name
+•	employees – first_name, last_name, job_title, salary
+Keep the ordering from the previous problem. Submit your query statements as Prepare DB & Run queries.
+*/
+
+SELECT name FROM towns
+ORDER BY name;
+SELECT name FROM departments
+ORDER BY name;
+SELECT first_name, last_name, job_title, salary FROM employees
+ORDER BY salary DESC;

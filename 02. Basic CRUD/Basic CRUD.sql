43ff27d5dde04990dd30397691371190342f9366
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:17:08 +0300

    commit solution of 10th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -86,3 +86,12 @@ one column called “Full Name”. Submit your query statements as Prepare DB & 
 SELECT concat_ws(' ',`first_name`, `middle_name`, `last_name`) 
 AS 'Full Name' FROM employees
 WHERE salary IN(25000, 14000, 12500, 23600);
+
+/*
+10.	Find All Employees Without Manager
+Write a SQL query to find first and last names about those employees that does not have a manager. 
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT first_name, last_name FROM employees
+WHERE manager_id IS NULL;

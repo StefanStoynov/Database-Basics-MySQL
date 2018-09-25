8edc3568f122fc81cf874dee548e53d7e05a10aa
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 11:37:27 +0300

    commit solution of 17th task

--- 01. Introduction To Databases/Intruduction to Databases.sql
+++ 01. Introduction To Databases/Intruduction to Databases.sql
@@ -519,3 +519,19 @@ Submit your query statements as Prepare DB & Run queries.
 SELECT * FROM towns;
 SELECT * FROM departments;
 SELECT * FROM employees;
+
+# 18.	Basic Select All Fields and Order Them
+/*
+Modify queries from previous problem by sorting:
+•	towns - alphabetically by name
+•	departments - alphabetically by name
+•	employees - descending by salary
+Submit your query statements as Prepare DB & Run queries.
+*/
+
+SELECT * FROM towns
+ORDER BY name;
+SELECT * FROM departments
+ORDER BY name;
+SELECT * FROM employees
+ORDER BY salary DESC;

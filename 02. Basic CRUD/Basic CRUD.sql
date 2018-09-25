36ea3253e3d3b70bfc7480385c558176b091d7ed
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:00:48 +0300

    commit solution of 7th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -54,3 +54,14 @@ Submit your query statements as Prepare DB & run queries.
 
 SELECT DISTINCT salary FROM employees
 ORDER BY employee_id;
+
+/*
+7.	Find all Information About Employees
+Write a SQL query to find all information about the employees whose job title is “Sales Representative”.
+Sort the information by id. Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT * FROM employees
+WHERE job_title = 'Sales Representative'
+ORDER BY employee_id;
+

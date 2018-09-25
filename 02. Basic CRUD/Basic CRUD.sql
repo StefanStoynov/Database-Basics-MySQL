51b02e6124d595811617ae372483bfc213679067
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 16:15:00 +0300

    commit solution of 19th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -185,3 +185,14 @@ Sort the information by id.  Submit your query statements as Prepare DB & run qu
 SELECT * FROM projects
 ORDER BY start_date, name
 LIMIT 10;
+
+/*
+19.	 Last 7 Hired Employees
+Write a SQL query to find last 7 hired employees. Select their first, last name and their hire date.
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT first_name, last_name, hire_date FROM employees
+ORDER BY hire_date DESC
+LIMIT 7;
+

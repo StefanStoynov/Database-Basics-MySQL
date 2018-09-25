b6a07b8a79da52ee2bc9ae3328923009434b3852
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 14:20:03 +0300

    commit solution of first task

---
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -0,0 +1,8 @@
+# 1.	Find All Information About Departments
+/*
+Write a SQL query to find all available information about the departments. 
+Sort the information by id. Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT * FROM departments
+ORDER BY department_id;

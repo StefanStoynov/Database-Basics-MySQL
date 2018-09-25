a968764aae49451efac15a08703b703e1ac8002e
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:25:07 +0300

    commit solution of 13th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -116,3 +116,12 @@ Submit your query statements as Prepare DB & run queries.
 SELECT first_name, last_name FROM employees
 ORDER BY salary DESC
 LIMIT 5;
+
+/*
+13.	Find All Employees Except Marketing
+Write a SQL query to find the first and last names of all employees whose department ID is different from 4. 
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT first_name, last_name FROM employees
+WHERE department_id != 4;

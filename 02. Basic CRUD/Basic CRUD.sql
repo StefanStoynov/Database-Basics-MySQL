f6e2a3da6f77ab2253850930638c4f9b7a2bf897
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 16:11:29 +0300

    commit solution of 18th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -174,3 +174,14 @@ Submit your query statements as Prepare DB & run queries.
 */
 
 SELECT DISTINCT job_title FROM employees;
+
+/*
+18.	Find First 10 Started Projects
+Write a SQL query to find first 10 started projects. 
+Select all information about them and sort them by start date, then by name. 
+Sort the information by id.  Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT * FROM projects
+ORDER BY start_date, name
+LIMIT 10;

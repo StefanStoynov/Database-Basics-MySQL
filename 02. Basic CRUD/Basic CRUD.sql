0e8e96c2171a834a83267e5633be92bf109313fa
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:30:51 +0300

    commit solution of 14th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -125,3 +125,20 @@ Submit your query statements as Prepare DB & run queries.
 
 SELECT first_name, last_name FROM employees
 WHERE department_id != 4;
+
+/*
+14.	Sort Employees Table
+Write a SQL query to sort all records in the еmployees table by the following criteria: 
+•	First by salary in decreasing order
+•	Then by first name alphabetically
+•	Then by last name descending
+•	Then by middle name alphabetically
+Sort the information by id. Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT * FROM employees
+ORDER BY salary DESC, 
+first_name,
+last_name DESC,
+middle_name,
+employee_id;

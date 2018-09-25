d715c415a100c39afc0b1993740dd447ffa347f0
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 14:37:07 +0300

    commit solution of 5th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -32,3 +32,15 @@ Submit your query statements as Prepare DB & run queries.
 
 SELECT first_name, middle_name, last_name FROM employees
 ORDER BY employee_id;
+
+/*
+5.	Find Email Address of Each Employee
+Write a SQL query to find the email address of each employee. (by his first and last name). 
+Consider that the email domain is softuni.bg. Emails should look like “John.Doe@softuni.bg". 
+The produced column should be named "full_ email_address". 
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT concat(`first_name`,'.',`last_name`,'@softuni.bg')
+ AS 'full_ email_address'
+ FROM employees;

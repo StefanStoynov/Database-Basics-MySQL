a808f28ddf2bd72e9f53d8097a7dc669e6f833f0
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 16:26:39 +0300

    commit solution of 20th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -196,3 +196,15 @@ SELECT first_name, last_name, hire_date FROM employees
 ORDER BY hire_date DESC
 LIMIT 7;
 
+/*
+20.	Increase Salaries
+Write a SQL query to increase salaries of all employees that are in the Engineering, Tool Design,
+Marketing or Information Services department by 12%. Then select Salaries column from the Employees table.
+Submit your query statements as Prepare DB & run queries.
+*/
+
+UPDATE employees
+SET salary = salary * 1.12
+WHERE department_id IN(1,2,4,11);
+
+SELECT salary FROM employees;

d9b234a1577d090b8efaf207f7fd8b75214c05c4
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:22:40 +0300

    commit solution of 12th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -106,3 +106,13 @@ Submit your query statements as Prepare DB & run queries.
 SELECT first_name, last_name, salary FROM employees
 WHERE salary > 50000
 ORDER BY salary DESC;
+
+/*
+12.	Find 5 Best Paid Employees
+Write SQL query to find first and last names about 5 best paid Employees ordered descending by their salary.
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT first_name, last_name FROM employees
+ORDER BY salary DESC
+LIMIT 5;

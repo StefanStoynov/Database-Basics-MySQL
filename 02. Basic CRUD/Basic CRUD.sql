9c4629c0167863c517a3c3858420e35d58334953
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-25 15:10:19 +0300

    commit solution of 9th task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -75,3 +75,14 @@ Submit your query statements as Prepare DB & run queries.
 SELECT first_name, last_name, job_title FROM employees
 WHERE salary BETWEEN 20000 and 30000
 ORDER BY employee_id;
+
+/*
+9.	 Find Names of All Employees 
+Write a SQL query to find the full name of all employees whose salary is 25000, 14000, 12500 or 23600. 
+Full Name is combination of first, middle and last name (separated with single space) and they should be in 
+one column called “Full Name”. Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT concat_ws(' ',`first_name`, `middle_name`, `last_name`) 
+AS 'Full Name' FROM employees
+WHERE salary IN(25000, 14000, 12500, 23600);

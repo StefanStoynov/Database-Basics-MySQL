6961004e96c994000ec96dfc180db0dbe2530d81
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-26 09:05:07 +0300

    commit solution of 23rd task

--- 02. Basic CRUD/Basic CRUD.sql
+++ 02. Basic CRUD/Basic CRUD.sql
@@ -229,3 +229,17 @@ SELECT country_name, population FROM countries
 WHERE continent_code = 'EU'
 ORDER BY population DESC, country_name
 LIMIT 30;
+
+/*
+23.	 Countries and Currency (Euro / Not Euro)
+Find all countries along with information about their currency. Display the country name, 
+country code and information about its currency: either "Euro" or "Not Euro". 
+Sort the results by country name alphabetically. Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT 
+country_name,
+country_code,
+(CASE WHEN currency_code !='EUR' THEN 'Not Euro' ELSE 'Euro' END) 
+FROM countries
+ORDER BY country_name;

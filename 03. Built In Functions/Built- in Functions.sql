5aa690ca6b3baa9186185b45e0d49e445849700d
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-27 16:46:44 +0300

    commit solution of last task from  lab.

--- 03. Built In Functions/Built- in Functions.sql
+++ 03. Built In Functions/Built- in Functions.sql
@@ -43,3 +43,12 @@ Submit your query statements as Prepare DB & run queries.
 SELECT concat(`first_name`, ' ', `last_name`) AS 'Full Name', 
 TIMESTAMPDIFF(DAY, born, died) AS 'Days Lived'
 FROM authors;
+
+/*
+5.	Harry Potter Books
+Write a SQL query to retrieve titles of all the Harry Potter books. 
+Order the information by id. Submit your query statements as Prepare DB & run queries. 
+*/
+
+SELECT title FROM books
+WHERE title LIKE 'Harry Potter%';

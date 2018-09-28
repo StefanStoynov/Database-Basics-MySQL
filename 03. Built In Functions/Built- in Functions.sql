bd496a9075d26f69bfc1af5639b02c7a1c336586
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-28 09:45:32 +0300

    commit solution of 4th task

--- 03. Built In Functions/Built- in Functions.sql
+++ 03. Built In Functions/Built- in Functions.sql
@@ -89,3 +89,13 @@ SELECT first_name FROM employees
 WHERE (department_id = 3 OR department_id = 10) 
 AND YEAR(hire_date) BETWEEN 1995 AND 2005
 ORDER BY employee_id;
+
+/*
+4.	Find All Employees Except Engineers
+Write a SQL query to find the first and last names of all employees whose job titles does not contain 
+“engineer”. Order by id. 
+Submit your query statements as Prepare DB & run queries.
+*/
+
+SELECT first_name, last_name FROM employees
+WHERE job_title NOT LIKE '%engineer%'; 

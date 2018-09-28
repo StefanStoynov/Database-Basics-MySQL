769e5e454f43a9452dde7687566fc8e038f27407
Stefan Stoynov <sstoynov87@gmail.com>
2018-09-28 13:30:07 +0300

    commit solution of 7th task

--- 03. Built In Functions/Built- in Functions.sql
+++ 03. Built In Functions/Built- in Functions.sql
@@ -121,3 +121,13 @@ Submit your query statements as Prepare DB & run queries.
 SELECT town_id, name FROM towns
 WHERE LEFT(name,1) IN ('M','K','B','E')
 ORDER BY name;
+
+/*
+7.	 Find Towns Not Starting With
+Write a SQL query to find all towns that does not start with letters R, B or D (case insensitively). 
+Order them alphabetically by name. 
+Submit your query statements as Prepare DB & run queries.
+*/
+SELECT town_id, name FROM towns
+WHERE name REGEXP'^[^R|B|D]+'
+ORDER BY name;

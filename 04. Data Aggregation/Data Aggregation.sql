1f5f14d745d0952f5d5bf3c1d855d7c308937cd3
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-01 14:24:45 +0300

    commit solution of 3rd task from lab

--- 04. Data Aggregation/Data Aggregation.sql
+++ 04. Data Aggregation/Data Aggregation.sql
@@ -20,3 +20,15 @@ Submit your queries with the MySQL prepare DB & run queries strategy.
 
 SELECT department_id, round(avg(salary),2) AS `Average Salary` FROM employees
 GROUP BY department_id;
+
+
+/*
+3.	 Min Salary
+Write a query to retrieve information about the departments grouped by department_id with minumum salary higher than 800.
+Round the salary result to two digits after the decimal point.
+Submit your queries with the MySQL prepare DB & run queries strategy.
+*/
+
+SELECT department_id,round(min(salary),2) AS `Min Salary` FROM employees
+GROUP BY department_id
+HAVING `Min Salary`>800;

81adaf8b2f0ab21c54a9958f0a2b4df985a9f18c
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-01 14:21:13 +0300

    commit solution of 2nd task from lab

--- 04. Data Aggregation/Data Aggregation.sql
+++ 04. Data Aggregation/Data Aggregation.sql
@@ -9,3 +9,14 @@ then by employees count. Submit your queries with the MySQL prepare DB & run que
 SELECT department_id, count(department_id) AS `count` FROM employees
 GROUP BY department_id
 ORDER BY department_id, `count`;
+
+/*
+2.	Average Salary
+Write a query to calculate the average salary in each department.
+Order the information by department_id.
+Round the salary result to two digits after the decimal point.
+Submit your queries with the MySQL prepare DB & run queries strategy.
+*/
+
+SELECT department_id, round(avg(salary),2) AS `Average Salary` FROM employees
+GROUP BY department_id;

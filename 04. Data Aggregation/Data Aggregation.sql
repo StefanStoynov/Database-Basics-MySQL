604c17b6a7ddb9608be8ad66e591bd7577ad93e7
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-02 14:40:12 +0300

    commit solution of 9th task

--- 04. Data Aggregation/Data Aggregation.sql
+++ 04. Data Aggregation/Data Aggregation.sql
@@ -170,6 +170,18 @@ The query should return:
 Sort result by increasing size of age groups.
 */
 
+SELECT CASE
+    WHEN age <= 10 THEN '[0-10]'
+    WHEN age <= 20 THEN '[11-20]'
+    WHEN age <= 30 THEN '[21-30]'
+    WHEN age <= 40 THEN '[31-40]'
+    WHEN age <= 50 THEN '[41-50]'
+    WHEN age <= 60 THEN '[51-60]'
+    ELSE '[61+]'
+    END AS `age_group`, count(id) AS 'wizzard_count' FROM wizzard_deposits
+GROUP BY `age_group`
+ORDER BY `age_group`;
+
 /*
 10. First Letter
 Write a query that returns all unique wizard first letters of their first names only if they have deposit

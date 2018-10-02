4e0f7463986e13ecb26cf05117271b223d182344
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-02 14:26:11 +0300

    commit solution of 8th task

--- 04. Data Aggregation/Data Aggregation.sql
+++ 04. Data Aggregation/Data Aggregation.sql
@@ -148,6 +148,11 @@ Group by deposit_group and magic_wand_creator.
 Select the data in ascending order by magic_wand_creator and deposit_group.
 */
 
+SELECT deposit_group, magic_wand_creator, min(deposit_charge) AS `min_deposit_charge`
+FROM wizzard_deposits
+GROUP BY deposit_group, magic_wand_creator
+ORDER BY magic_wand_creator, deposit_group;
+
 /*
 9. Age Groups
 Write down a query that creates 7 different groups based on their age.

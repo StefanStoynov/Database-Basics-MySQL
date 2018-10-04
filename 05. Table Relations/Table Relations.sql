a697b7c48bd9bc1e8c593c19579bf03b04672247
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-04 15:37:11 +0300

    commit solution of 4th task from lab

--- 05. Table Relations/Table Relations.sql
+++ 05. Table Relations/Table Relations.sql
@@ -68,6 +68,13 @@ Get information about the hiking routes and their leaders – name and id.
 Submit your queries using the “MySQL prepare DB and Run Queries” strategy.
 */
 
+SELECT starting_point AS 'route_starting_point',
+       end_point AS 'route_ending_point',
+       leader_id,
+       concat(first_name,' ', last_name) AS 'leader_name'
+FROM routes
+JOIN campers ON leader_id = campers.id;
+
 /*
 5.	 Project Management DB*
 Write a query to create a project management db according to the following E/R Diagram:

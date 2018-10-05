c6af80422f847a06cdcfbe55cebc313bb6cb2c15
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-05 14:46:13 +0300

    commit solution of 5th task

--- 05. Table Relations/Table Relations.sql
+++ 05. Table Relations/Table Relations.sql
@@ -240,6 +240,49 @@ ALTER TABLE teachers
 5.	Online Store Database
 */
 
+CREATE TABLE customers(
+  customer_id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  birthday DATE,
+  city_id INT NOT NULL
+);
+
+CREATE TABLE cities(
+  city_id INT PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+ALTER TABLE customers
+    ADD CONSTRAINT fk_customers_cities FOREIGN KEY (city_id) REFERENCES cities(city_id);
+
+CREATE TABLE orders(
+  order_id INT PRIMARY KEY,
+  customer_id INT NOT NULL,
+  CONSTRAINT fk_orders_customers FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE items(
+  item_id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NUll,
+  item_type_id INT NOT NULL
+);
+
+CREATE TABLE item_types(
+  item_type_id INT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE items
+    ADD CONSTRAINT fk_items_item_types FOREIGN KEY (item_type_id) REFERENCES item_types(item_type_id);
+
+CREATE TABLE order_items(
+  order_id INT,
+  item_id INT,
+  CONSTRAINT pk_order_item PRIMARY KEY (order_id, item_id),
+  CONSTRAINT fk_order_items_items FOREIGN KEY (item_id) REFERENCES items(item_id),
+  CONSTRAINT fk_order_items_orders FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
+
 /*
 6.	University Database
 */

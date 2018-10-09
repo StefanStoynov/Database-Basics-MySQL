b2f9580d5dd007ed734be822dff05b98f0e1d86c
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-09 11:42:01 +0300

    commit solution of 4th task

--- 06. Subqueries And Joins/Subqueries And Joins.sql
+++ 06. Subqueries And Joins/Subqueries And Joins.sql
@@ -105,6 +105,13 @@ Write a query that selects:
 Filter only employees with salary higher than 15000. Return the first 5 rows sorted by department_id in descending order.
 */
 
+SELECT e.employee_id,e.first_name,e.salary,d.name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE e.salary > 15000
+ORDER BY d.department_id DESC
+LIMIT 5;
+
 /*5.	Employees Without Project
 Write a query that selects:
 •	employee_id

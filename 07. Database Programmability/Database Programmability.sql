333828db66fad4ea79b8996cd7f8012cde54a711
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-12 10:41:42 +0300

    commit solution of last task from lab

--- 07. Database Programmability/Database Programmability.sql
+++ 07. Database Programmability/Database Programmability.sql
@@ -57,6 +57,24 @@ Add a trigger to employees table that inserts the corresponding information in d
 Submit your queries using the “MySQL Run Skeleton, run queries and check DB” strategy.
 */
 
+CREATE TABLE deleted_employees(
+  employee_id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL ,
+  last_name VARCHAR(50)NOT NULL ,
+  middle_name VARCHAR(50),
+  job_title VARCHAR(50),
+  department_id INT(10),
+  salary DECIMAL(19,4));
+
+CREATE TRIGGER tr_deleted_employees
+  AFTER DELETE
+  ON employees
+  FOR EACH ROW
+  BEGIN
+    INSERT INTO deleted_employees(first_name, last_name, middle_name, job_title, department_id, salary)
+    VALUES (OLD.first_name,OLD.last_name,OLD.middle_name,OLD.job_title,OLD.department_id,OLD.salary);
+  END $$
+
 #Exercises: Functions, Triggers and Transactions
 
 

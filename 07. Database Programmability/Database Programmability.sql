5fa3b971d3d27971933793ae93681ca0be051eec
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-12 11:04:49 +0300

    commit solution of 2nd task

--- 07. Database Programmability/Database Programmability.sql
+++ 07. Database Programmability/Database Programmability.sql
@@ -100,6 +100,15 @@ first and last names whose salary is above or equal to the given number. The res
 then by last_name alphabetically and id ascending. Submit your query statement as Run skeleton, run queries & check DB in Judge.
 */
 
+CREATE PROCEDURE usp_get_employees_salary_above(number DOUBLE)
+  BEGIN
+    SELECT e.first_name,e.last_name FROM employees e
+        WHERE e.salary >= number
+    ORDER BY e.first_name,e.last_name,e.employee_id;
+  END $$
+
+CALL usp_get_employees_salary_above(30000);
+
 /*3.	Town Names Starting With
 Write a stored procedure usp_get_towns_starting_with that accept string as parameter and returns all town names
 starting with that string. The result should be sorted by town_name alphabetically. Submit your query statement as

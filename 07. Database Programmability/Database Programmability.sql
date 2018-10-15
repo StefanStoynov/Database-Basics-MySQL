21b0d4cd895f122f7975e55591d677786a86b47c
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-15 15:15:01 +0300

    commit solution of 13th task

--- 07. Database Programmability/Database Programmability.sql
+++ 07. Database Programmability/Database Programmability.sql
@@ -318,7 +318,21 @@ the specified precision.
 Submit your query statement as Run skeleton, run queries & check DB in Judge.
 */
 
+CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(20,4))
+  BEGIN
+    START TRANSACTION ;
+    CASE WHEN money_amount < 0 OR money_amount > (SELECT balance FROM accounts a WHERE a.id = account_id )
+      THEN ROLLBACK;
+    ELSE UPDATE accounts a
+    SET a.balance = a.balance - money_amount
+      WHERE a.id = account_id;
+      END CASE ;
+    COMMIT ;
+  END $$
 
+CALL usp_withdraw_money (1, 10);
+select * from accounts
+WHERE id = 1;
 
 /*14.	Money Transfer
 Write stored procedure usp_transfer_money(from_account_id, to_account_id, amount) that transfers money

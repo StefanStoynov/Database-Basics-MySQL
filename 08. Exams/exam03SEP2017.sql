c926251d6c10c4e7033bad2c9faebd7682ecc9a0
Stefan Stoynov <sstoynov87@gmail.com>
2018-10-18 12:02:15 +0300

    commit solution of 3rd task from exam 03SEP2017

--- 08. Exams/exam03SEP2017.sql
+++ 08. Exams/exam03SEP2017.sql
@@ -51,6 +51,19 @@ WHERE p.id BETWEEN 1 and 10;
 
 # 03.	Data Update
 
+UPDATE users AS u
+JOIN  users_followers AS uf
+ON u.id = uf.user_id
+    JOIN
+        (SELECT uf.user_id, count(uf.follower_id) AS `folowers_count` FROM  users_followers AS uf
+        GROUP BY uf.user_id
+        ORDER BY uf.user_id) AS `folowers`
+        ON u.id = `folowers`.user_id
+    SET u.profile_picture_id = CASE WHEN `folowers`.`folowers_count`= 0 THEN u.id
+    ELSE `folowers`.`folowers_count`
+    END
+WHERE u.profile_picture_id IS NULL;
+
 # 04.	Data Deletion
 
 DELETE u
